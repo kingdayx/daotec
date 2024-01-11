6e06faf4c1bc272672594ba2fb04d94f824cd6c7
kingdayx <54965488+kingdayx@users.noreply.github.com>
2024-01-11 13:02:13 -0500

    changed first slider color

--- src/components/Services/page.js
+++ src/components/Services/page.js
@@ -16,12 +16,12 @@ export default function Services() {
       {isMobile ? (
         <div>
           <Swiper
-            spaceBetween={50}
+            spaceBetween={30}
             slidesPerView={3}
             onSlideChange={() => console.log("slide change")}
             onSwiper={(swiper) => console.log(swiper)}
           >
-            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide style={{ color: "black" }}>Slide 1</SwiperSlide>
             <SwiperSlide>Slide 2</SwiperSlide>
             <SwiperSlide>Slide 3</SwiperSlide>
             <SwiperSlide>Slide 4</SwiperSlide>
