8d3604f22e17beaf08035c15e124df02b2358eca
kingdayx <54965488+kingdayx@users.noreply.github.com>
2024-01-11 12:51:53 -0500

    added swiper

--- src/components/Services/page.js
+++ src/components/Services/page.js
@@ -3,12 +3,30 @@ import Image from "next/image";
 import * as services from "../../../public/assets/services.png";
 import * as CGI from "../../../public/assets/CGI.svg";
 import { isMobile } from "react-device-detect";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
 
 export default function Services() {
   return (
     <div className="servicesContainer">
       {isMobile ? (
-        <div> mobile device </div>
+        <div>
+          <Swiper
+            spaceBetween={50}
+            slidesPerView={3}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>Slide 4</SwiperSlide>
+            ...
+          </Swiper>
+        </div>
       ) : (
         <div>
           <div className="servicesInfo">
